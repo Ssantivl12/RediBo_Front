7af8b2bc28edf08b216a640f17686256cce04484
Diego <diegodelgadillo2024@gmail.com>
2025-04-07 16:33:24 -0400

    componente Notificacion

--- realizar-pagos/src/main.jsx
+++ realizar-pagos/src/main.jsx
@@ -1,11 +1,12 @@
-// src/main.jsx
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Notificacion from './componentes/Notificacion'
+
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css'; // Importa Tailwind
+import Notificacion from './componentes/Notificacion';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <React.StrictMode>
     <Notificacion />
-  </StrictMode>
-)
+  </React.StrictMode>
+);
