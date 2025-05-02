e331458bc5657d9a9dbdfa370629b5a626833874
bladi123x <zeballosbladimir@gmail.com>
2025-05-02 14:38:41 -0400

    tarea #55 carpetas TituloDescripcion y gestionar

---
+++ my-app/src/app/gestionar/page.tsx
@@ -0,0 +1,11 @@
+import TituloDescripcion from "@components/TituloDescripcion/TituloDescripcion";
+
+export default function GestionarSolicitudes() {
+  return (
+    <div>
+      <TituloDescripcion />
+      {/* Resto del contenido de la página */}
+    </div>
+  );
+}
+
