80253ee1d59950299fa8f0a1a1c003adc1d5522b
Pierre-Rodri <solizcespedesrodrigo@gmail.com>
2025-06-10 19:55:41 -0400

    fix: se muestra alerta si la respuesta del backend es usuario no encontrado

--- my-app/src/app/components/auth/authRegistroHost/VehicleDataModal.tsx
+++ my-app/src/app/components/auth/authRegistroHost/VehicleDataModal.tsx
@@ -380,5 +380,5 @@ const VehicleDataModal: React.FC<Props> = ({ onNext, onClose }) => {
     </div>
   );
 };
-// Exportamos el componente VehicleDataModal
+// Exportamos el componente VehicleDataModall
 export default VehicleDataModal;
