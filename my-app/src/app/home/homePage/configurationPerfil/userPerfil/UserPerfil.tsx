1ae2f4048af70a222c84fa49bd73e46462693832
Melina Montaño <202200060@est.umss.edu>
2025-06-11 00:56:41 -0400

    aplicación de estilos correctos al boton visualizar drivers

--- my-app/src/app/home/homePage/configurationPerfil/userPerfil/UserPerfil.tsx
+++ my-app/src/app/home/homePage/configurationPerfil/userPerfil/UserPerfil.tsx
@@ -36,7 +36,7 @@ export default function UserPerfilPage() {
       <div className="relative">
         <button
           onClick={() => setShowModal(true)}
-          className="absolute top-4 right-6 bg-orange-500 text-white font-semibold px-4 py-2 rounded-full shadow-md hover:bg-orange-600 transition"
+          className="absolute top-4 right-6 bg-[#FFA800] text-white font-semibold px-4 py-2 rounded-full shadow-md hover:bg-[#ff8c00] transition duration-200 cursor-pointer"
         >
           Lista de Drivers
         </button>
